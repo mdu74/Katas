b75da08f73facce4fca5a23bab1cc1fefe8f85ce
Mdu Cwele <Mdu@chilli.local>
2016-04-06 10:54:12 +0200

    Added test to check if three is Fizz

--- FizzBuzz_2016_04_06/FizzBuzzKata/FizzBuzzKata/FizzBuzz.cs
+++ FizzBuzz_2016_04_06/FizzBuzzKata/FizzBuzzKata/FizzBuzz.cs
@@ -1,9 +1,15 @@
-﻿namespace FizzBuzzKata
+﻿using System;
+
+namespace FizzBuzzKata
 {
     public class FizzBuzz
     {
         public string IsFizzBuzz(string input)
         {
+            if(int.Parse(input) == 3)
+            {
+                return "Fizz";
+            }
             return input;
 
         }
--- FizzBuzz_2016_04_06/FizzBuzzKata/FizzBuzzKata/TestFizzBuzz.cs
+++ FizzBuzz_2016_04_06/FizzBuzzKata/FizzBuzzKata/TestFizzBuzz.cs
@@ -9,7 +9,7 @@ namespace FizzBuzzKata
         public void IsFizzBuzz_GivenOne_ShouldReturnOne()
         {
             //---------------Set up test pack-------------------
-            var fizzBuzz = new FizzBuzz();
+            var fizzBuzz = CreateFizzBuzz();
             var input = "1";
             var expected = "1";
             //---------------Assert Precondition----------------
@@ -25,7 +25,7 @@ namespace FizzBuzzKata
         public void IsFizzBuzz_GivenTwo_ShouldReturnTwo()
         {
             //---------------Set up test pack-------------------
-            var fizzBuzz = new FizzBuzz();
+            var fizzBuzz = CreateFizzBuzz();
             var input = "2";
             var expected = "2";
             //---------------Assert Precondition----------------
@@ -35,5 +35,26 @@ namespace FizzBuzzKata
             //---------------Test Result -----------------------
             Assert.AreEqual(expected, result);
         }
+
+        [Test]
+        public void IsFizzBuzz_GivenThree_ShouldReturnFizz()
+        {
+            //---------------Set up test pack-------------------
+            var fizzBuzz = CreateFizzBuzz();
+            var input = "3";
+            var expected = "Fizz";
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = fizzBuzz.IsFizzBuzz(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result);
+        }
+
+        private static FizzBuzz CreateFizzBuzz()
+        {
+            return new FizzBuzz();
+        }
+
     }
 }
