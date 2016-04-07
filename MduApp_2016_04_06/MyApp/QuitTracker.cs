35116e69394b58ccc94ee47724ce2727ec1a59f4
Mdu Cwele <Mdu@chilli.local>
2016-04-07 16:03:04 +0200

    Added QuitTracker delegates

---
+++ MduApp_2016_04_06/MyApp/QuitTracker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp
+{
+    public class QuitTracker
+    {
+        public string MyTracker { get; set; }
+
+        public void QuitHandler()
+        {
+            Console.WriteLine("My name is {0}, I've just recieved a quite alert.", MyTracker);
+        }
+    }
+}
