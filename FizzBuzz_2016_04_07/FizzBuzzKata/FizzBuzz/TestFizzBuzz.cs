360beaed10dc03596d62b834a3bdd4feb3947bf7
Mdu Cwele <Mdu@chilli.local>
2016-04-08 08:19:31 +0200

    Refactored test for numbers NOT divisible by three and five.

--- FizzBuzz_2016_04_07/FizzBuzzKata/FizzBuzz/TestFizzBuzz.cs
+++ FizzBuzz_2016_04_07/FizzBuzzKata/FizzBuzz/TestFizzBuzz.cs
@@ -6,43 +6,14 @@ namespace FizzBuzz
     [TestFixture]
     public class TestFizzBuzz
     {
-        [Test]
-        public void IsFizzBuzz_GivenOne_ShouldReturnOne()
+        [TestCase("1")]
+        [TestCase("2")]
+        [TestCase("4")]
+        public void IsFizzBuzz_GivenNumberNoneDivisibleByThreeAndFive_ShouldReturnNumber(string input)
         {
             //---------------Set up test pack-------------------
             var fizzBuzz = CreateFizzBuzz();
-            var input = "1";
-            var expected = "1";
-            //---------------Assert Precondition----------------
-
-            //---------------Execute Test ----------------------
-            var result = fizzBuzz.IsFizzBuzz(input);
-            //---------------Test Result -----------------------
-            Assert.AreEqual(expected, result);
-        }
-
-        [Test]
-        public void IsFizzBuzz_GivenTwo_ShouldReturnTwo()
-        {
-            //---------------Set up test pack-------------------
-            var fizzBuzz = CreateFizzBuzz();
-            var input = "2";
-            var expected = "2";
-            //---------------Assert Precondition----------------
-
-            //---------------Execute Test ----------------------
-            var result = fizzBuzz.IsFizzBuzz(input);
-            //---------------Test Result -----------------------
-            Assert.AreEqual(expected, result);
-        }
-
-        [Test]
-        public void IsFizzBuzz_GivenThree_ShouldReturnFizz()
-        {
-            //---------------Set up test pack-------------------
-            var fizzBuzz = CreateFizzBuzz();
-            var input = "3";
-            var expected = "Fizz";
+            var expected = input;
             //---------------Assert Precondition----------------
 
             //---------------Execute Test ----------------------
