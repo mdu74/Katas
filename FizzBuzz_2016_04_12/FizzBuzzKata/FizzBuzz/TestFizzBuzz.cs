58d663a5e0d689023536c0856bdfecef8fb066a4
Mdu Cwele <Mdu@chilli.local>
2016-04-12 13:29:22 +0200

    Added test to check if input One returns One

---
+++ FizzBuzz_2016_04_12/FizzBuzzKata/FizzBuzz/TestFizzBuzz.cs
@@ -0,0 +1,24 @@
+﻿
+using NUnit.Framework;
+
+namespace FizzBuzz
+{
+    [TestFixture]
+    class TestFizzBuzz
+    {
+        [Test]
+        public void IsFizzBuzz_GivenOne_ShouldReturnOne()
+        {
+            //---------------Set up test pack-------------------
+            var fizzBuzz = new FizzBuzz();
+            var expected = "1";
+            var input = "1";
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = fizzBuzz.IsFizzBuzz(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
