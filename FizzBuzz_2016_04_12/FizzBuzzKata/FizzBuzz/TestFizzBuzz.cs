897c93f5e260038e6a778a0d5b4e76169048c5fa
Mdu Cwele <Mdu@chilli.local>
2016-04-12 13:42:40 +0200

    Added test to check if input of Any number returns Number

--- FizzBuzz_2016_04_12/FizzBuzzKata/FizzBuzz/TestFizzBuzz.cs
+++ FizzBuzz_2016_04_12/FizzBuzzKata/FizzBuzz/TestFizzBuzz.cs
@@ -20,5 +20,25 @@ namespace FizzBuzz
             //---------------Test Result -----------------------
             Assert.AreEqual(expected, result);
         }
+
+        [Test]
+        public void Add_GivenAnyNumber_ShouldReturnNumber()
+        {
+            //---------------Set up test pack-------------------
+            var fizzBuzz = CreateFizzBuzz();
+            var expected = "2";
+            var input = "2";
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = fizzBuzz.IsFizzBuzz(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result);
+        }
+
+        private static FizzBuzz CreateFizzBuzz()
+        {
+            return new FizzBuzz();
+        }
     }
 }
