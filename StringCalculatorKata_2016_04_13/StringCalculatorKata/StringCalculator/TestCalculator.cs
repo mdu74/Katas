271f7d3925d2fdb57f2ac260ed40e175cfc143e3
Mdu Cwele <Mdu@chilli.local>
2016-04-13 08:43:45 +0200

    Added test to check if Empty input returns Zero

---
+++ StringCalculatorKata_2016_04_13/StringCalculatorKata/StringCalculator/TestCalculator.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace StringCalculator
+{
+    [TestFixture]
+    class TestCalculator
+    {
+        [Test]
+        public void Add_GivenNoNumbers_ShouldReturnZero()
+        {
+            //---------------Set up test pack-------------------
+            var calculator = new Calculator();
+            var numbers = "";
+            var expected = 0;
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = calculator.Add(numbers);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
