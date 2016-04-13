b14efec589c971b0af7f12fee412ef4668fab96c
Mdu Cwele <Mdu@chilli.local>
2016-04-13 08:57:34 +0200

    Added test to check if Single number return Number

--- StringCalculatorKata_2016_04_13/StringCalculatorKata/StringCalculator/TestCalculator.cs
+++ StringCalculatorKata_2016_04_13/StringCalculatorKata/StringCalculator/TestCalculator.cs
@@ -9,7 +9,7 @@ namespace StringCalculator
         public void Add_GivenNoNumbers_ShouldReturnZero()
         {
             //---------------Set up test pack-------------------
-            var calculator = new Calculator();
+            var calculator = CreateCalculator();
             var numbers = "";
             var expected = 0;
             //---------------Assert Precondition----------------
@@ -18,6 +18,26 @@ namespace StringCalculator
             var result = calculator.Add(numbers);
             //---------------Test Result -----------------------
             Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Add_GivenSingle_ShouldReturnNumber()
+        {
+            //---------------Set up test pack-------------------
+            var calculator = CreateCalculator();
+            var expected = 2;
+            var numbers = "2";
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = calculator.Add(numbers);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result);
+        }
+
+        private static Calculator CreateCalculator()
+        {
+            return new Calculator();
         }
     }
 }
