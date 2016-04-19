b8f30327a3e7d9b83af419b126b4bfe1a1086932
Mdu Cwele <Mdu@chilli.local>
2016-04-19 08:37:00 +0200

    Added test to check if Single number returns the Number

---
+++ StringCalculatorKata_2016_04_19/StringCalculatorKata/StringCalculator/Calculator.cs
@@ -0,0 +1,14 @@
+﻿namespace StringCalculator
+{
+    public class Calculator
+    {
+        public int Add(string numbers)
+        {
+            if (numbers == "")
+            {
+                return 0;
+            }
+            return int.Parse(numbers);
+        }
+    }
+}
---
+++ StringCalculatorKata_2016_04_19/StringCalculatorKata/StringCalculator/TestCalculator.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+
+namespace StringCalculator
+{
+    [TestFixture]
+    class TestCalculator
+    {
+        [Test]
+        public void Add_GivenNoNumbers_ShouldReturnZero()
+        {
+            //---------------Set up test pack-------------------
+            var calculator = new Calculator();
+            var numbers = "";
+            var expected = 0;
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = calculator.Add(numbers);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Add_GivenSingleNumber_ShouldReturnNumber()
+        {
+            //---------------Set up test pack-------------------
+            var calculator = CreateCalculator();
+            var numbers = "1";
+            var expected = 1;
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = calculator.Add(numbers);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result);
+        }
+
+        private static Calculator CreateCalculator()
+        {
+            return new Calculator();
+        }
+    }
+}
