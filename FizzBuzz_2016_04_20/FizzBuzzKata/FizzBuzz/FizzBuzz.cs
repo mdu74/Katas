d082ac7272b0dfe1898ae80562ffc917f1f79d92
Mdu Cwele <Mdu@chilli.local>
2016-04-20 14:49:36 +0200

    Added test to check if input of Numbers Divisible By Three returns Fizz

--- FizzBuzz_2016_04_20/FizzBuzzKata/FizzBuzz/FizzBuzz.cs
+++ FizzBuzz_2016_04_20/FizzBuzzKata/FizzBuzz/FizzBuzz.cs
@@ -10,11 +10,10 @@ namespace FizzBuzz
     {
         public string IsFizzBuzz(string input)
         {
-            if (input == "3")
+            if (int.Parse(input)%3 == 0)
             {
                 return "Fizz";
             }
-
             if (input == "5")
             {
                 return "Buzz";
--- FizzBuzz_2016_04_20/FizzBuzzKata/FizzBuzz/TestFizzBuzz.cs
+++ FizzBuzz_2016_04_20/FizzBuzzKata/FizzBuzz/TestFizzBuzz.cs
@@ -70,6 +70,21 @@ namespace FizzBuzz
             Assert.AreEqual(expected, result);
         }
 
+        [Test]
+        public void IsFizzBuzz_GivenNumberDivisibleByThree_ShouldReturnFizz()
+        {
+            //---------------Set up test pack-------------------
+            var fizzBuzz = CreateFizzBuzz();
+            var input = "6";
+            var expected = "Fizz";
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = fizzBuzz.IsFizzBuzz(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result);
+        }
+
         private static FizzBuzz CreateFizzBuzz()
         {
             return new FizzBuzz();
