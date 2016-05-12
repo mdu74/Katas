a4a45a6ae52c4bace023852e9e783957406eb9e8
Mdu Cwele <Mdu@chilli.local>
2016-05-12 09:29:33 +0200

    Completed in 23 minutes

---
+++ StringCalculatorKata_2016_05_12/StringCalculatorKata/StringCalculator/TestCalculator.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace StringCalculator
+{
+    [TestFixture]
+    class TestCalculator
+    {
+        [Test]
+        public void Add_GivenEmptyString_ShouldReturnZero()
+        {
+            //---------------Set up test pack-------------------
+            var calculator = new Calculator();
+            var numbers = "";
+            var expected = 0;
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = calculator.Add(numbers);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result);
+        }
+        [Test]
+        public void Add_GivenSingleNumbers_ShouldReturnNumber()
+        {
+            //---------------Set up test pack-------------------
+            var calculator = CreateCalculator();
+            var numbers = "1";
+            var expected = 1;
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = calculator.Add(numbers);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Add_GivenTwoNumbers_ShouldReturnSum()
+        {
+            //---------------Set up test pack-------------------
+            var calculator = CreateCalculator();
+            var numbers = "1,2";
+            var expected = 3;
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = calculator.Add(numbers);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Add_GivenMultipleNumbers_ShouldReturnSum()
+        {
+            //---------------Set up test pack-------------------
+            var calculator = CreateCalculator();
+            var numbers = "1,2,3";
+            var expected = 6;
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = calculator.Add(numbers);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Add_GivenNewLineBetweenNumbers_ShouldReturnSum()
+        {
+            //---------------Set up test pack-------------------
+            var calculator = CreateCalculator();
+            var numbers = "1\n2,3";
+            var expected = 6;
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = calculator.Add(numbers);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Add_GivenStartsWithASeparateLineDelimiter_ShouldReturnSum()
+        {
+            //---------------Set up test pack-------------------
+            var calculator = CreateCalculator();
+            var numbers = "//;\n1;2";
+            var expected = 3;
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = calculator.Add(numbers);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Add_GivenNegativeNumbers_ShouldReturnErrorMessage()
+        {
+            //---------------Set up test pack-------------------
+            var calculator = CreateCalculator();
+            var numbers = "-1,2,-3";
+            var expected = "Negatives not allowed";
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = Assert.Throws<Exception>(()=> calculator.Add(numbers));
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result.Message);
+        }
+
+        [Test]
+        public void Add_GivenGreaterThanAThousand_ShouldIgnore()
+        {
+            //---------------Set up test pack-------------------
+            var calculator = CreateCalculator();
+            var numbers = "1001,2";
+            var expected = 2;
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = calculator.Add(numbers);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Add_GivenAnyLengthDelimiter_ShouldReturnSum()
+        {
+            //---------------Set up test pack-------------------
+            var calculator = CreateCalculator();
+            var numbers = "//[***]\n1***2***3";
+            var expected = 6;
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = calculator.Add(numbers);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Add_GivenMultipleDelimiter_ShouldReturnSum()
+        {
+            //---------------Set up test pack-------------------
+            var calculator = CreateCalculator();
+            var numbers = "//[*][%]\n1*2%3";
+            var expected = 6;
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = calculator.Add(numbers);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result);
+        }
+
+        private static Calculator CreateCalculator()
+        {
+            return new Calculator();
+        }
+    }
+}
