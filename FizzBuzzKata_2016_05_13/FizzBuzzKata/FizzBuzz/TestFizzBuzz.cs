8c0da806831474322b739d2889e5b687648b4e56
Mdu Cwele <Mdu@chilli.local>
2016-05-16 10:33:15 +0200

    Completed in 22 minutes

---
+++ FizzBuzzKata_2016_05_13/FizzBuzzKata/FizzBuzz/TestFizzBuzz.cs
@@ -0,0 +1,118 @@
+﻿using NUnit.Framework;
+
+namespace FizzBuzz
+{
+    [TestFixture]
+    class TestFizzBuzz
+    {
+        [Test]
+        public void IsFizzBuzz_GivenOne_ShouldReturnOne()
+        {
+            //---------------Set up test pack-------------------
+            var fizzBuzz = CreateFizzBuzz();
+            var input = "1";
+            var expected = "1";
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = fizzBuzz.IsFizzBuzz(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void IsFizzBuzz_GivenNotDivisibleByEitherThreeOrFive_ShouldReturnNumber()
+        {
+            //---------------Set up test pack-------------------
+            var fizzBuzz = CreateFizzBuzz();
+            var input = "2";
+            var expected = "2";
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = fizzBuzz.IsFizzBuzz(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void IsFizzBuzz_GivenThree_ShouldReturnFizz()
+        {
+            //---------------Set up test pack-------------------
+            var fizzBuzz = CreateFizzBuzz();
+            var input = "3";
+            var expected = "Fizz";
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = fizzBuzz.IsFizzBuzz(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void IsFizzBuzz_GivenFive_ShouldReturnBuzz()
+        {
+            //---------------Set up test pack-------------------
+            var fizzBuzz = CreateFizzBuzz();
+            var input = "5";
+            var expected = "Buzz";
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = fizzBuzz.IsFizzBuzz(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void IsFizzBuzz_GivenDivisibleByThree_ShouldReturnFizz()
+        {
+            //---------------Set up test pack-------------------
+            var fizzBuzz = CreateFizzBuzz();
+            var input = "6";
+            var expected = "Fizz";
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = fizzBuzz.IsFizzBuzz(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void IsFizzBuzz_GivenDivisibleByFive_ShouldReturnBuzz()
+        {
+            //---------------Set up test pack-------------------
+            var fizzBuzz = CreateFizzBuzz();
+            var input = "10";
+            var expected = "Buzz";
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = fizzBuzz.IsFizzBuzz(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void IsFizzBuzz_GivenDivisibleByBothThreeAndFive_ShouldReturnFizzBuzz()
+        {
+            //---------------Set up test pack-------------------
+            var fizzBuzz = CreateFizzBuzz();
+            var input = "15";
+            var expected = "FizzBuzz";
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = fizzBuzz.IsFizzBuzz(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result);
+        }
+
+        private static FizzBuzz CreateFizzBuzz()
+        {
+            return new FizzBuzz();
+        }
+    }
+}
